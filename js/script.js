b6074676f6856f12947fcc7ffdc394d02c6a43a0
Shadedog838 <Shandonmith@gmail>
2021-03-14 22:23:51 -0400

    fixed bug

--- js/script.js
+++ js/script.js
@@ -15,470 +15,470 @@ var answers=0;
 
 $('.one').click(function(){
     click1=click1+1;
-     if(click1===1){       
-   $('.one').css('background', '#FFFF00');  
+     if(click1===1){
+   $('.one').css('background', '#FFFF00');
     userscore=userscore+30;
      answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-    
+
 });
 $('.one2').click(function(){
-   
+
    click2=click2+1;
-     if(click2===1){       
-   $('.one2').css('background', '#FFFF00');  
+     if(click2===1){
+   $('.one2').css('background', '#FFFF00');
     userscore=userscore+30;
      answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.one3').click(function(){
-   
+
  click3=click3+1;
-     if(click3===1){       
-   $('.one3').css('background', '#FFFF00');  
+     if(click3===1){
+   $('.one3').css('background', '#FFFF00');
     userscore=userscore+30;
      answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.one4').click(function(){
-   
+
   click4=click4+1;
-     if(click4===1){       
-   $('.one4').css('background', '#FFFF00');  
+     if(click4===1){
+   $('.one4').css('background', '#FFFF00');
     userscore=userscore+30;
      answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.one5').click(function(){
-   
+
  click5=click5+1;
-     if(click5===1){       
-   $('.one5').css('background', '#FFFF00');  
+     if(click5===1){
+   $('.one5').css('background', '#FFFF00');
     userscore=userscore+30;
      answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.one6').click(function(){
-   
+
     click6=click6+1;
-     if(click6===1){       
-   $('.one6').css('background', '#FFFF00');  
+     if(click6===1){
+   $('.one6').css('background', '#FFFF00');
     userscore=userscore+30;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.one7').click(function(){
-   
+
   click7=click7+1;
-     if(click7===1){       
-   $('.one7').css('background', '#FFFF00');  
+     if(click7===1){
+   $('.one7').css('background', '#FFFF00');
     userscore=userscore+30;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 
 
 $('.two').click(function(){
     click1=click1+1;
-     if(click1===1){       
-   $('.two').css('background', '#BA55D3');  
+     if(click1===1){
+   $('.two').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-    
+
 });
 $('.two2').click(function(){
     click2=click2+1;
-     if(click2===1){       
-   $('.two2').css('background', '#BA55D3');  
+     if(click2===1){
+   $('.two2').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-  
-    
+
+
 });
 
 $('.two3').click(function(){
-   
+
  click3=click3+1;
-     if(click3===1){       
-   $('.two3').css('background', '#BA55D3');  
+     if(click3===1){
+   $('.two3').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-    
+
 });
 $('.two4').click(function(){
-   
+
  click4=click4+1;
-     if(click4===1){       
-   $('.two4').css('background', '#BA55D3');  
+     if(click4===1){
+   $('.two4').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
 });
 $('.two5').click(function(){
-   
+
  click5=click5+1;
-     if(click5===1){       
-   $('.two5').css('background', '#BA55D3');  
+     if(click5===1){
+   $('.two5').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-    
+
 });
 $('.two6').click(function(){
-   
+
  click6=click6+1;
-     if(click6===1){       
-   $('.two6').css('background', '#BA55D3');  
+     if(click6===1){
+   $('.two6').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 });
 $('.two7').click(function(){
-   
+
  click7=click7+1;
-     if(click7===1){   
-   $('.two7').css('background', '#BA55D3');  
+     if(click7===1){
+   $('.two7').css('background', '#BA55D3');
     userscore=userscore+20;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 
-    
+
 });
 
-  
-    
-    
+
+
+
 
 $('.three').click(function(){
    click1=click1+1;
-     if(click1===1){   
-      $('.three').css('background', '#ADD8E6'); 
+     if(click1===1){
+      $('.three').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-  
-    
+
+
 });
 $('.three2').click(function(){
-   
+
    click2=click2+1;
-     if(click2===1){   
-      $('.three2').css('background', '#ADD8E6'); 
+     if(click2===1){
+      $('.three2').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.three3').click(function(){
-   
+
    click3=click3+1;
-     if(click3===1){   
-      $('.three3').css('background', '#ADD8E6'); 
+     if(click3===1){
+      $('.three3').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 });
 $('.three4').click(function(){
-   
+
    click4=click4+1;
-     if(click4===1){   
-      $('.three4').css('background', '#ADD8E6'); 
+     if(click4===1){
+      $('.three4').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
 });
 $('.three5').click(function(){
-   
+
    click5=click5+1;
-     if(click5===1){   
-      $('.three5').css('background', '#ADD8E6'); 
+     if(click5===1){
+      $('.three5').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.three6').click(function(){
-   
+
    click6=click6+1;
-     if(click6===1){   
-      $('.three6').css('background', '#ADD8E6'); 
+     if(click6===1){
+      $('.three6').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.three7').click(function(){
-   
+
    click7=click7+1;
-     if(click7===1){   
-      $('.three7').css('background', '#ADD8E6'); 
+     if(click7===1){
+      $('.three7').css('background', '#ADD8E6');
     userscore=userscore+40;
     answers=answers+1;
- 
+
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 
 $('.four').click(function(){
    click1=click1+1;
-     if(click1===1){   
+     if(click1===1){
       $('.four').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.four2').click(function(){
-   
+
  click2=click2+1;
-     if(click2===1){   
+     if(click2===1){
       $('.four2').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.four3').click(function(){
-   
+
    click3=click3+1;
-     if(click3===1){   
+     if(click3===1){
       $('.four3').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.four4').click(function(){
-   
+
 click4=click4+1;
-     if(click4===1){   
+     if(click4===1){
       $('.four4').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.four5').click(function(){
-   
+
   click5=click5+1;
-     if(click5===1){   
+     if(click5===1){
       $('.four5').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.four6').click(function(){
-   
+
  click6=click6+1;
-     if(click6===1){   
+     if(click6===1){
       $('.four6').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
  $('.four7').click(function(){
-   
+
 click7=click7+1;
-     if(click7===1){   
+     if(click7===1){
       $('.four7').css('background', '#6A5ACD');
     userscore=userscore+0;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 
 $('.five').click(function(){
-   
+
    click1=click1+1;
-     if(click1===1){   
+     if(click1===1){
      $('.five').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.five2').click(function(){
-   
+
 click2=click2+1;
-     if(click2===1){   
+     if(click2===1){
      $('.five2').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.five3').click(function(){
-   
+
 click3=click3+1;
-     if(click3===1){   
+     if(click3===1){
      $('.five3').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.five4').click(function(){
-   
+
   click4=click4+1;
-     if(click4===1){   
+     if(click4===1){
      $('.five4').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.five5').click(function(){
-   
+
    click5=click5+1;
-     if(click5===1){   
+     if(click5===1){
      $('.five5').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 
 $('.five6').click(function(){
-   
+
    click6=click6+1;
-     if(click6===1){   
+     if(click6===1){
      $('.five6').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 $('.five7').click(function(){
-   
+
    click7=click7+1;
-     if(click7===1){   
+     if(click7===1){
      $('.five7').css('background', '#DB7093');
     userscore=userscore+10;
     answers=answers+1;
      }else{
-       alert('You already answered'); 
+       alert('You already answered');
      }
     console.log(userscore);
-    
+
 });
 function result(){
     if(userscore<=50){
@@ -504,12 +504,16 @@ function result(){
 }
 }
 $('#button').click(function(){
- 
+
  if(answers===7){
     result();
  $('#button').fadeOut();
  }else{
   alert('Please answer all questions');
  }
-  
+
 });
+
+$('#again').click(function() {
+  location.reload();
+})
